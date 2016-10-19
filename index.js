cc05b9e3dfbbec7ea253f8e680c63a544ccafdbb
LanguageGuru <guru.chandratrichy@isobar.com>
2016-10-19 20:29:57 +1100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ if (token) {
 }
 
 controller.hears('Test', ['mention'], function(Opal) {
-	  var self = Opal.top, $scope = Opal, nil = Opal.nil, $breaker = Opal.breaker, $slice = Opal.slice, session = nil, ws = nil;
+/*	  var self = Opal.top, $scope = Opal, nil = Opal.nil, $breaker = Opal.breaker, $slice = Opal.slice, session = nil, ws = nil;
 
 	  Opal.add_stubs(['$require', '$login', '$[]', '$worksheets', '$spreadsheet_by_key', '$[]=', '$save', '$reload', '$puts', '$i']);
 	  self.$require("rubygems");
@@ -39,7 +39,7 @@ controller.hears('Test', ['mention'], function(Opal) {
 	  ws['$[]='](2, 1, "=gTranslate(\"this is a test\", \"en\", \"es\")");
 	  ws.$save();
 	  ws.$reload();
-	  return self.$puts(ws['$[]'](self.$i(), 1));
+	  return self.$puts(ws['$[]'](self.$i(), 1));*/
 	  bot.reply(message, "TEST EXECUTED")
 	  (Opal);
 	})
