ee337f848f87779ac5cf5754e82288d2c855aee1
LanguageGuru <guru.chandratrichy@isobar.com>
2016-10-19 20:27:59 +1100

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ if (token) {
   require('beepboop-botkit').start(controller, { debug: true })
 }
 
-controller.hears('Test', ['mention'], function (bot, message) {
-	(function(Opal) {
+controller.hears('Test', ['mention'], function(Opal) {
 	  var self = Opal.top, $scope = Opal, nil = Opal.nil, $breaker = Opal.breaker, $slice = Opal.slice, session = nil, ws = nil;
 
 	  Opal.add_stubs(['$require', '$login', '$[]', '$worksheets', '$spreadsheet_by_key', '$[]=', '$save', '$reload', '$puts', '$i']);
@@ -42,7 +41,7 @@ controller.hears('Test', ['mention'], function (bot, message) {
 	  ws.$reload();
 	  return self.$puts(ws['$[]'](self.$i(), 1));
 	  bot.reply(message, "TEST EXECUTED")
-	})(Opal);
+	  (Opal);
 	})
 
 controller.on('bot_channel_join', function (bot, message) {
