4770c38ca3245923f951413090a899bacf438b58
LanguageGuru <guru.chandratrichy@isobar.com>
2016-10-19 17:33:15 +1100

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'It\'s nice to talk to you directly.')
 })
 
+controller.hears(['How are you', 'Hows things'], ['direct_message'], function (bot, message) {
+  bot.reply(message, 'Good Thanks!')
+  bot.reply(message, 'Fine Thank You!')
+})
+
 controller.hears('.*', ['mention'], function (bot, message) {
   bot.reply(message, 'You really do care about me. :heart:')
 })
@@ -75,3 +80,4 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 })
+
