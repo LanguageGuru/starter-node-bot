bd02206ccd3af2964d76f1d6e3c7775873eb3717
LanguageGuru <guru.chandratrichy@isobar.com>
2016-10-20 21:13:40 +1100

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ controller.hears('Translate', ['direct_mention'], function (bot, message)
 			bot.reply(message, 'sheet 1: '+sheet.title)
 			bot.reply(message, translate)
 			    
-  		        ws = session.getActiveSheet('Translator')['$[]'](0);
+  		        ws = session.getActiveSheet('LanguageSheet')['$[]'](0);
                         //ws['$[]='](2, 1, "=gTranslate(\"this is a test\", \"en\", \"es\")");
                         //ws.$save();
                         //ws.$reload();
